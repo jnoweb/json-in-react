aa04fd71b6cc404c6b0437c190a29d4cecec06aa
Johnny Nolan <i.am@jnoweb.com>
2019-05-06 20:28:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor ()
+  {
+    super();
+    this.state = {
+      data: [
+        {
+          id: 1,
+          name: 'kerstin',
+        },
+        {
+          id: 2,
+          name: 'kitty',
+        },
+        {
+          id: 3,
+          name: 'eve',
+        }
+      ]
+    }
+  }
+  render() 
+  {
+    return (
+      <div>
+        {
+          this.state.data.map((dynamicData, i) =>
+          <div style={{ display: 'flex' }}>
+            <div style={{ marginLeft: 10 }}>{dynamicData.id}</div>
+            <div style={{ marginLeft: 10 }}>{dynamicData.name}</div>
+          </div>
+          )
+        }
+      </div>
+    )
+  }
+
 }
 
 export default App;
